fb2a5d969d1deabdfdb45ea276d1abf8e21ac57a
Alejandro Salas <asalas.wd@outlook.com>
2019-11-10 21:50:57 -0500

    complete subtitle creation in frontend

--- src/components/SubtitleCreation.js
+++ src/components/SubtitleCreation.js
@@ -1,11 +1,16 @@
 import React from 'react';
 // import Project from './Project'
-// import axios from "axios";
+import axios from "axios";
 
 class SubtitleCreation extends React.Component {
-  state = {
-    subInit: false
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      subInit: false
+    }
   }
+  
     // function to create each subtitle
     createSub = () => {
       let tracks = document.querySelector('video').textTracks;
@@ -20,23 +25,49 @@ class SubtitleCreation extends React.Component {
       // if inTime has already been defined, set cue endTime to current video time and pause video
       } else {
         let outTime = video.currentTime;
-        let cuesLength = tracks[0].cues.length;
-        console.log('cuesLength = ' + cuesLength)
+        let cuesLength = tracks[0].cues.length;        
         video.pause();
         tracks[0].cues[cuesLength - 1].endTime = outTime;
-        console.log(tracks[0]);
-        console.log(tracks[0].cues[cuesLength - 1]);
         this.setState({subInit: false });
         // call function to display modal and enter text subtitle
         this.completeSub();
       }
-    }
+    };
 
     // function to display subtitle text modal after endTime has been set
     completeSub = () => {
       let modal = document.getElementById('sub-text');
       modal.style.display = 'block';      
-    }
+    };
+
+    // function to save subtitle with Save button in modal
+    saveSubtitle = () => {
+      let modal = document.getElementById('sub-text');
+      let tracks = document.querySelector('video').textTracks;
+      let video = document.getElementById('video');
+      let cuesLength = tracks[0].cues.length;
+      // set cue text to the text typed in the modal
+      let theText = document.getElementById('this-sub-text').value;
+      tracks[0].cues[cuesLength - 1].text = theText;
+      // clear modal text
+      document.getElementById('this-sub-text').value = '';
+      video.play();
+      modal.style.display = 'none';
+    };
+
+    // function to cancel and clear current subtitle sith Cancel button in modal
+    cancelSubtitle = () => {
+      let modal = document.getElementById('sub-text');
+      let tracks = document.querySelector('video').textTracks;
+      let video = document.getElementById('video');
+      let cuesLength = tracks[0].cues.length;
+      // delete current cue
+      tracks[0].removeCue(tracks[0].cues[cuesLength - 1]);
+      // clear modal text
+      document.getElementById('this-sub-text').value = '';
+      video.play();
+      modal.style.display = 'none';
+    };
 
   render() {
 
@@ -48,10 +79,10 @@ class SubtitleCreation extends React.Component {
 
         {/* Modal content */}
         <div id="sub-modal">
-            <span id="close">&times;</span>
+            <span id="close" onClick={this.cancelSubtitle}>&times;</span>
             <textarea id="this-sub-text" rows="2" cols="50" maxLength="80"></textarea>
-            <button id="save-text-btn">Save</button>
-            <button id="cancel-btn">Cancel</button>
+            <button id="save-text-btn" onClick={this.saveSubtitle}>Save</button>
+            <button id="cancel-btn" onClick={this.cancelSubtitle}>Cancel</button>
         </div>
 
       </div>
